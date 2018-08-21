c6e962f1ee658defe294420450d443898e54a980
Ashwani Solanki <solanki.ashwani@yahoo.com>
2018-08-21 16:27:17 +0530

    Some cleanup changes

--- src/main/java/com/jwt/controller/BookStoreController.java
+++ src/main/java/com/jwt/controller/BookStoreController.java
@@ -2,10 +2,10 @@ package com.jwt.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,84 +13,74 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.jwt.model.BookStore;
-
 import com.jwt.service.BookStoreService;
-
 
 @RestController
 public class BookStoreController {
 
-	private static final Logger logger = Logger
-			.getLogger(BookStoreController.class);
-
-	public BookStoreController() {
-		System.out.println("BookStoreController()");
-	}
+	private static final Logger logger = Logger.getLogger(BookStoreController.class);
 
 	@Autowired
 	private BookStoreService BookStoreService;
-	
 
-	@RequestMapping(value = "/",method = RequestMethod.GET)
-	public ModelAndView listAllBooks(ModelAndView model) throws IOException {
-		List<BookStore> bookStoreList = BookStoreService.getAllBooks();
-		BookStore bookStore = new BookStore();
-		model.addObject("bookStore", bookStore);
-		model.addObject("bookStoreList", bookStoreList);
-		model.setViewName("home");
+	@RequestMapping(value = RestConstants.PATH_SEPARATOR, method = RequestMethod.GET)
+	public ModelAndView listAllBooks(final ModelAndView model) throws IOException {
+		final List<BookStore> bookStoreList = BookStoreService.getAllBooks();
+		final BookStore bookStore = new BookStore();
+		model.addObject(RestConstants.BOOK_STORE, bookStore);
+		model.addObject(RestConstants.BOOK_STORE_LIST, bookStoreList);
+		model.setViewName(RestConstants.HOME);
 		return model;
 	}
 
-	@RequestMapping(value = "/newBook", method = RequestMethod.GET)
-	public ModelAndView newContact(ModelAndView model) {
-		BookStore book = new BookStore();
-		model.addObject("book", book);
-		model.setViewName("BookForm");
+	@RequestMapping(value = RestConstants.NEW_BOOK, method = RequestMethod.GET)
+	public ModelAndView newContact(final ModelAndView model) {
+		final BookStore book = new BookStore();
+		model.addObject(RestConstants.BOOK, book);
+		model.setViewName(RestConstants.BOOK_FORM);
 		return model;
 	}
 
-	@RequestMapping(value = "/saveBook", method = RequestMethod.POST)
-	public ModelAndView saveBook(@ModelAttribute BookStore book ) {
-		if (book.getId() == 0) { 
-	            
-	            
-			
+	@RequestMapping(value = RestConstants.SAVE_BOOK, method = RequestMethod.POST)
+	public ModelAndView saveBook(@ModelAttribute final BookStore book) {
+		if (book.getId() == 0) {
+
 			BookStoreService.addBook(book);
-		} else 
+		} else {
 			BookStoreService.updateBook(book);
-		
-		return new ModelAndView("redirect:/");
-		//return new ModelAndView("home");
+		}
+
+		return new ModelAndView(RestConstants.REDIRECT);
 	}
 
-	@RequestMapping(value = "/deleteBook", method = RequestMethod.GET)
-	public ModelAndView deleteBook(HttpServletRequest request) {
-		int bookId = Integer.parseInt(request.getParameter("id"));
+	@RequestMapping(value = RestConstants.DELETE_BOOK, method = RequestMethod.GET)
+	public ModelAndView deleteBook(final HttpServletRequest request) {
+		final int bookId = Integer.parseInt(request.getParameter("id"));
 		BookStoreService.deleteBook(bookId);
-		return new ModelAndView("redirect:/");
+		return new ModelAndView(RestConstants.REDIRECT);
 	}
 
-	@RequestMapping(value = "/editBook", method = RequestMethod.GET)
-	public ModelAndView editBook(HttpServletRequest request) {
-		int bookId = Integer.parseInt(request.getParameter("id"));
-		BookStore book = BookStoreService.getBook(bookId);
-		ModelAndView model = new ModelAndView("BookForm");
-		model.addObject("book", book);
+	@RequestMapping(value = RestConstants.EDIT_BOOK, method = RequestMethod.GET)
+	public ModelAndView editBook(final HttpServletRequest request) {
+		final int bookId = Integer.parseInt(request.getParameter(RestConstants.ID));
+		final BookStore book = BookStoreService.getBook(bookId);
+		final ModelAndView model = new ModelAndView(RestConstants.BOOK_FORM);
+		model.addObject(RestConstants.BOOK, book);
 
 		return model;
 	}
 
-	@RequestMapping(value="/home/{pageid}")  
-    public ModelAndView edit(@PathVariable int pageid){  
-        int total=10;  
-        if(pageid==1){}  
-        else{  
-            pageid=(pageid-1)*total+1;  
-        }  
-        List<BookStore> list = BookStoreService.getAllBooksByPage(pageid,total);  
-       
-        
-        return new ModelAndView("home","list",list);  
-    }  
+	@RequestMapping(value = "/home/{pageid}")
+	public ModelAndView edit(@PathVariable int pageid) {
+		final int total = 10;
+		if (pageid == 1) {
+		} else {
+			pageid = ((pageid - 1) * total) + 1;
+		}
+		final List<BookStore> list = BookStoreService.getAllBooksByPage(pageid, total);
+
+		return new ModelAndView("home", "list", list);
+	}
 }
---
+++ src/main/java/com/jwt/controller/RestConstants.java
@@ -0,0 +1,18 @@
+package com.jwt.controller;
+
+public final class RestConstants {
+
+	public static final String PATH_SEPARATOR = "/";
+	public static final String BOOK_STORE = "bookStore";
+	public static final String BOOK_STORE_LIST = "bookStoreList";
+	public static final String BOOK_FORM = "BookForm";
+	public static final String BOOK = "Book";
+	public static final String HOME = "home";
+	public static final String NEW_BOOK = PATH_SEPARATOR + "newBook";
+	public static final String EDIT_BOOK = PATH_SEPARATOR + "editBook";
+	public static final String SAVE_BOOK = PATH_SEPARATOR + "saveBook";
+	public static final String REDIRECT = "redirect:/";
+	public static final String DELETE_BOOK = "deleteBook";
+	public static final String ID = "id";
+
+}
